c0bbd82b78d15680d26e4a963a685cb89ff7890f
Mallaury <mallaury.ratajczyk@gmail.com>
2024-09-20 12:01:19 +0200

    corr

--- routes/year.js
+++ routes/year.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', (req, res) => {
-    let date = new Date().getFullYear();
+    let date = new Date().getFullYear().toString();
     console.log(date);
     res.json({ year: date });
 })
